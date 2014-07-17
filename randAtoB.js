e7a5a582dfa831997128441254fb161547aafd73
Harry Moreno <morenoh149@gmail.com>
2014-07-17 01:51:41 -0700

    typo

--- randAtoB.js
+++ randAtoB.js
@@ -1,4 +1,4 @@
-// takes to numbers and produces a random number between them
+// takes two numbers and produces a random number between them
 var randAtoB = function(a,b) {
  var res = a + (Math.random() * b-a ); 
  return res;
